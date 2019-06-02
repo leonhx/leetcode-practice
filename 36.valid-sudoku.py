aa625e92701261254ba123620eb4c6d6b42099c6
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-06-02 12:11:00 +0800

    remove debug

--- 36.valid-sudoku.py
+++ 36.valid-sudoku.py
@@ -4,9 +4,6 @@
 # [36] Valid Sudoku
 #
 class Solution:
-    def __init__(self, debug=False):
-        self.debug = debug
-
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = [[None] * 9 for _ in range(9)]
         cols = [[None] * 9 for _ in range(9)]
@@ -18,22 +15,12 @@ class Solution:
                 if c != '.':
                     n = ord(c) - code_bias
                     if rows[i][n]:
-                        if self.debug:
-                            print(f'row {i}: {c}; {i}, {j}')
                         return False
                     if cols[j][n]:
-                        if self.debug:
-                            print(f'col {j}: {c}; {i}, {j}')
                         return False
                     if boxs[i // 3][j // 3][n]:
-                        if self.debug:
-                            print(f'box {i // 3} {j // 3}: {c}; {i}, {j}')
                         return False
                     rows[i][n] = True
                     cols[j][n] = True
                     boxs[i // 3][j // 3][n] = True
-                    if self.debug:
-                        print(f'got row {i}: {c}')
-                        print(f'got col {j}: {c}')
-                        print(f'got box {i // 3} {j // 3}: {c}')
         return True
