7f01eedf0c57a9bdac60c944c1c3e52fd6c46c3d
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-06-02 15:42:22 +0800

    37: WA

--- 37.sudoku-solver.py
+++ 37.sudoku-solver.py
@@ -18,28 +18,11 @@ class Solution:
         self.cols[j][n - 1] = present
         self.boxs[i // 3][j // 3][n - 1] = present
 
-    def init(self) -> None:
-        for i in range(9):
-            for j in range(9):
-                xij = self.board[i][j]
-                if xij != '.':
-                    self._mark(i, j, int(xij), True)
-
-    def backtrace(self):
-        i, j = self.decision_seq[self.seq_i]
-        self.seq_i -= 1
+    def reset(self, i: int, j: int) -> None:
         self._mark(i, j, int(self.board[i][j]), False)
         self.board[i][j] = '.'
-        return i, j
 
     def update(self, i: int, j: int, n: int) -> None:
-        if len(self.decision_seq) - 1 > self.seq_i:
-            while len(self.decision_seq) - 2 > self.seq_i:
-                self.attempts.pop(self.decision_seq.pop(), None)
-            self.decision_seq.pop()
-        self.decision_seq.append((i, j))
-        self.seq_i += 1
-        self.attempts.setdefault((i, j), set()).add(n)
         self._mark(i, j, n, True)
         self.board[i][j] = str(n)
 
@@ -51,10 +34,10 @@ class Solution:
                     missing.append((i, j))
         return missing
 
-    def propose(self, indices):
+    def propose(self, indices, blacklist={}):
         candidates = {}
         for i, j in indices:
-            blackset = self.attempts.get((i, j), set())
+            blackset = blacklist.get((i, j), set())
             ns = [n for n in range(1, 10)
                   if self.isValid(i, j, n) and n not in blackset]
             if ns:
@@ -62,7 +45,29 @@ class Solution:
         return candidates
 
     def solve(self) -> bool:
-        pass
+        missing_indices = self.findMissing()
+        if not missing_indices:
+            return True
+        candidates = self.propose(missing_indices)
+        if not candidates:
+            return False
+        updated = False
+        for (i, j), ns in candidates.items():
+            if len(ns) == 1:
+                self.update(i, j, ns[0])
+                updated = True
+        if updated:
+            return self.solve()
+        else:
+            (i, j), ns = sorted(list(candidates.items()),
+                                key=lambda x: len(x[1]))[0]
+            for n in ns:
+                self.update(i, j, n)
+                solved = self.solve()
+                if solved:
+                    return True
+                self.reset(i, j)
+            return False
 
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
@@ -72,28 +77,9 @@ class Solution:
         self.rows = [[None] * 9 for _ in range(9)]
         self.cols = [[None] * 9 for _ in range(9)]
         self.boxs = [[[None] * 9 for _ in range(3)] for _ in range(3)]
-        self.decision_seq = []
-        self.seq_i = -1
-        self.attempts = {}
-        self.init()
-        missing_indices = self.findMissing()
-
-        while missing_indices:
-            candidates = self.propose(missing_indices)
-            if candidates:
-                updated = False
-                for (i, j), ns in candidates.items():
-                    if len(ns) == 1:
-                        print(f'confident set {i} {j} to {ns[0]}')
-                        self.update(i, j, ns[0])
-                        updated = True
-                if not updated:
-                    (i, j), ns = sorted(list(candidates.items()),
-                                        key=lambda x: len(x[1]))[0]
-                    print(f'try set {i} {j} to {ns[0]}')
-                    self.update(i, j, ns[0])
-            else:  # no candidates, backtrace now
-                i, j = self.backtrace()
-                print(f'backtrace {i} {j}')
-            missing_indices = self.findMissing()
-            print('---')
+        for i in range(9):
+            for j in range(9):
+                xij = self.board[i][j]
+                if xij != '.':
+                    self._mark(i, j, int(xij), True)
+        self.solve()
