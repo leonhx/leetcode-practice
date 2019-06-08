75a3cdb917edb99ed701a1b016aade736f12fb9a
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-06-08 14:33:28 +0800

    44: simpler and clearer solution

--- 44.wildcard-matching.py
+++ 44.wildcard-matching.py
@@ -4,97 +4,32 @@
 # [44] Wildcard Matching
 #
 class Solution:
-    def _parse(self, p: str) -> List[str]:
-        i, ps = 0, []
-        while i < len(p):
-            if p[i] == '*':
-                ps.append('*')
-                while i < len(p) and p[i] == '*':
-                    i += 1
-            elif p[i] == '?':
-                j = i + 1
-                while j < len(p) and p[j] == '?':
-                    j += 1
-                ps.append(p[i:j])
-                i = j
-            else:
-                j = i + 1
-                while j < len(p) and p[j] != '*' and p[j] != '?':
-                    j += 1
-                ps.append(p[i:j])
-                i = j
-        return ps
-
-    def _to_groups(self, p: List[str]):
-        if not p:
-            return []
-        groups = []
-        if p[0][0] != '*':
-            groups.append([None, p[0]])
-        else:
-            groups.append([])
-        for w in p[1:]:
-            if w[0] == '*' and groups[-1]:
-                groups.append([])
-            else:
-                groups[-1].append(w)
-        if groups[-1]:
-            groups[-1].append(None)
-        else:
-            groups.pop()
-        return groups
-
-    def _len(self, group: List[str]):
-        return sum(len(w) for w in group if w)
-
-    def _check(self, s: str, group: List[str], start: int) -> int:
-        is_valid = True
-        for w in group:
-            if w is None:
-                return start == len(s)
-            elif w[0] == '?':
-                is_valid = start + len(w) <= len(s)
-            else:
-                is_valid = s[start:].startswith(w)
-            if not is_valid:
-                break
-            start += len(w)
-        return is_valid
-
-    def _find(self, s: str, group: List[str], start: int) -> int:
-        if group[0] is None:
-            if start != 0:
-                return False
-            return 0 if self._check(s, group[1:], 0) else -1
-        bias = 0
-        if group[0][0] == '?':
-            bias += len(group[0])
-            group = group[1:]
-        start += bias
-        while start <= len(s) - self._len(group):
-            if group and group[0]:
-                i = s.find(group[0], start)
-                if i == -1:
-                    return -1
-                j = i + len(group[0])
-            else:
-                j = i = start
-            if self._check(s, group[1:], j):
-                return i - bias
-            start = i + 1
-        return -1
+    def _consume_seq(self, s: str, p: str, s_i: int, p_i: int):
+        while p_i < len(p) and p[p_i] != '*':
+            if s_i >= len(s) or (p[p_i] != s[s_i] and p[p_i] != '?'):
+                return -1, -1
+            s_i, p_i = s_i + 1, p_i + 1
+        if p_i == len(p) and s_i != len(s): return -1, -1
+        return s_i, p_i
 
     def isMatch(self, s: str, p: str) -> bool:
         if not p:
             return not s
-        p_index = self._parse(p)
-        groups = self._to_groups(p_index)
-        i = 0
-        for group in groups:
-            i = self._find(s, group, i)
-            if i == -1:
-                return False
-            i += self._len(group)
-            if i > len(s):
-                return False
-        return True
+        s_i, p_i = 0, 0
+        if p[p_i] != '*' and p[p_i] != '?':
+            s_i, p_i = self._consume_seq(s, p, s_i, p_i)
+            if s_i == -1: return False
+        could_skip = False
+        while p_i < len(p):
+            if p[p_i] == '*':
+                p_i, could_skip = p_i + 1, True
+            elif p[p_i] == '?':
+                s_i, p_i = s_i + 1, p_i + 1
+            else:
+                s_i_, p_i_ = self._consume_seq(s, p, s_i, p_i)
+                if s_i_ == -1:
+                    if could_skip and s_i < len(s) - 1: s_i += 1
+                    else: return False
+                else:
+                    s_i, p_i, could_skip = s_i_, p_i_, False
+        return (could_skip and s_i <= len(s)) or s_i == len(s)
