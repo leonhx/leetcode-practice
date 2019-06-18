422dad41d3c84dd6d4f8eb69d1900e82c3cfbb0f
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-06-18 22:52:11 +0800

    57: AC, better runtime & memory

--- 57.insert-interval.py
+++ 57.insert-interval.py
@@ -4,19 +4,32 @@
 # [57] Insert Interval
 #
 class Solution:
+    def overlap(self, interval_a: List[int], interval_b: List[int]) -> bool:
+        a_st, a_et = interval_a
+        b_st, b_et = interval_b
+        return a_st <= b_st <= a_et or a_st <= b_et <= a_et or \
+            (b_st <= a_st and b_et >= a_et)
+
+    def merge(self, interval_a: List[int], interval_b: List[int]) -> bool:
+        return [min([interval_a[0], interval_b[0]]), max([interval_a[1], interval_b[1]])]
+
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         if not intervals:
             return [newInterval]
         mid_i = len(intervals) // 2
-        mid_st, mid_et = intervals[mid_i]
-        new_st, new_et = newInterval
-        if mid_st <= new_st <= mid_et or mid_st <= new_et <= mid_et or \
-                (new_st <= mid_st and new_et >= mid_et):
-            newIntrvl = [min([mid_st, new_st]), max([mid_et, new_et])]
-            return self.insert(intervals[:mid_i] + intervals[mid_i + 1:], newIntrvl)
-        elif new_et < mid_st:
+        if self.overlap(intervals[mid_i], newInterval):
+            newInterval = self.merge(intervals[mid_i], newInterval)
+            i, j = mid_i - 1, mid_i + 1
+            while i >= 0 and self.overlap(intervals[i], newInterval):
+                newInterval = self.merge(intervals[i], newInterval)
+                i -= 1
+            while j < len(intervals) and self.overlap(intervals[j], newInterval):
+                newInterval = self.merge(intervals[j], newInterval)
+                j += 1
+            return self.insert(intervals[:i + 1] + intervals[j:], newInterval)
+        elif newInterval[1] < intervals[mid_i][0]:
             return self.insert(intervals[:mid_i], newInterval) + intervals[mid_i:]
-        elif new_st > mid_et:
+        elif newInterval[0] > intervals[mid_i][1]:
             return intervals[:mid_i + 1] + self.insert(intervals[mid_i + 1:], newInterval)
         else:
             raise RuntimeError('should not reach here')
