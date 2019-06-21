f5db81ef82a1da3a795452328d3a73fed8e8549c
(Leon) X. Huang <leonhx@users.noreply.github.com>
2019-06-21 21:52:21 +0800

    61: AC. rt 87.3 %, mem 43.34 %

---
+++ 61.rotate-list.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode id=61 lang=python3
+#
+# [61] Rotate List
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def length(self, head: ListNode) -> int:
+        return 1 + self.length(head.next) if head else 0
+
+    def length2(self, head: ListNode) -> int:
+        n = 0
+        while head:
+            head = head.next
+            n += 1
+        return n
+
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        n = self.length(head)
+        if n == 0:
+            return head
+        k %= n
+        if k == 0:
+            return head
+        node = head
+        for _ in range(n - k - 1):
+            node = node.next
+        new_head, node.next = node.next, None
+        node = new_head
+        while node.next:
+            node = node.next
+        node.next = head
+        return new_head
