8d19061ed4fe14343335270c99c7a69603f2b395
(Leon) X. Huang <leonhx@users.noreply.github.com>
2020-08-04 22:47:49 +0800

    84: TLE again

--- 84.largest-rectangle-in-histogram.py
+++ 84.largest-rectangle-in-histogram.py
@@ -6,7 +6,7 @@
 
 # @lc code=start
 class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
+    def largestRectangleArea_1(self, heights: List[int]) -> int:
         if not heights:
             return 0
         if len(heights) == 1:
@@ -17,7 +17,30 @@ class Solution:
                 min_i = i
                 min_h = h
         area = min_h * len(heights)
-        left_max = self.largestRectangleArea(heights[:min_i])
-        right_max = self.largestRectangleArea(heights[min_i + 1:])
+        left_max = self.largestRectangleArea_1(heights[:min_i])
+        right_max = self.largestRectangleArea_1(heights[min_i + 1:])
         return max([area, left_max, right_max])
+
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        if not heights:
+            return 0
+        total_num = len(heights)
+        if total_num == 1:
+            return heights[0]
+        max_area = heights[0]
+        for i in range(total_num):
+            min_h = heights[i]
+            if min_h > max_area:
+                max_area = min_h
+            for j in range(i + 1, total_num):
+                if heights[j] < min_h:
+                    area = (j - i) * min_h
+                    if area > max_area:
+                        max_area = area
+                    min_h = heights[j]
+                if j == total_num - 1:
+                    area = (j - i + 1) * min_h
+                    if area > max_area:
+                        max_area = area
+        return max_area
 # @lc code=end
