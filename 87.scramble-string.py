a1651650eb8fcf3b7cf8f02d0cd0fb5f8d8d93c5
(Leon) X. Huang <leonhx@users.noreply.github.com>
2020-08-07 22:24:15 +0800

    87: AC

--- 87.scramble-string.py
+++ 87.scramble-string.py
@@ -5,27 +5,37 @@
 #
 
 # @lc code=start
+from collections import Counter
+
+
 class Solution:
-    def find(self, x1: str, x2: str, s2: List[str]):
-        for i in range(len(s2) - 1):
-            if (s2[i] == x1 and s2[i + 1] == x2) or (
-                    s2[i] == x2 and s2[i + 1] == x1):
-                yield i
-
-    def search(self, s1: List[str], s2: List[str]) -> bool:
-        if len(s1) == len(s2) == 1:
-            return s1[0] == s2[0]
-        for i in range(len(s1) - 1):
-            for j in self.find(s1[i], s1[i + 1], s2):
-                hit = s1[i] + s1[i + 1]
-                return self.search(s1[:i] + [hit] + s1[i + 2:],
-                                   s2[:j] + [hit] + s2[j + 2:])
-        return False
-
     def isScramble(self, s1: str, s2: str) -> bool:
         if not s1:
             return not s2
         if len(s1) != len(s2):
             return False
-        return self.search(list(s1), list(s2))
+        if s1 == s2:
+            return True
+        s1_counter = Counter(s1)
+        s2_counter = Counter(s2)
+        if s1_counter != s2_counter:
+            return False
+        s1_left_c = Counter()
+        s2_left_c = Counter()
+        s2_right_c = Counter()
+        for i in range(1, len(s1)):
+            s1_left_c.update(s1[i - 1])
+            s2_left_c.update(s2[i - 1])
+            s2_right_c.update(s2[len(s2) - i])
+            if s1_left_c == s2_left_c and \
+                    (s1_counter - s1_left_c) == (s2_counter - s2_left_c):
+                if self.isScramble(s1[:i], s2[:i]) and \
+                        self.isScramble(s1[i:], s2[i:]):
+                    return True
+            elif s1_left_c == s2_right_c and \
+                    (s1_counter - s1_left_c) == (s2_counter - s2_right_c):
+                if self.isScramble(s1[:i], s2[-i:]) and \
+                        self.isScramble(s1[i:], s2[:-i]):
+                    return True
+        return False
 # @lc code=end
